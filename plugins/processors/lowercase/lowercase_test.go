a52011345d4f36fda4f0cceb241f2813813e9e8a
Philip Norman <philip@mesosphere.io>
2018-10-16 11:25:33 -0700

    Test that lowercase metrics are not duplicated
    With SendOriginals enabled, metrics with names or fields which contain uppercase
    characters should be duplicated. However, metrics which do not should not be.

--- plugins/processors/lowercase/lowercase_test.go
+++ plugins/processors/lowercase/lowercase_test.go
@@ -6,64 +6,75 @@ import (
 	"testing"
 	"time"
 
+	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/metric"
 	"github.com/stretchr/testify/assert"
 )
 
-// By default, we don't send original metrics, only lowercased metrics
-func TestApply_Defaults(t *testing.T) {
-	input, err := metric.New(
-		"tEsT",
-		map[string]string{},
-		map[string]interface{}{
+var (
+	fields = map[string]map[string]interface{}{
+		"ChAnGeD": map[string]interface{}{
 			"lower_case": "abc123",
 			"UPPER_CASE": "ABC123",
 			"Mixed_Case": "Abc123",
 		},
-		time.Now(),
-	)
-	assert.Nil(t, err)
+		"unchanged": map[string]interface{}{
+			"lower_case": "abc123",
+		},
+	}
+)
+
+// By default, we don't send original metrics, only lowercased metrics
+func TestApply_Defaults(t *testing.T) {
+	inputs := make([]telegraf.Metric, 2)
+	inputs[0], _ = metric.New("ChAnGeD", map[string]string{}, fields["ChAnGeD"], time.Now())
+	inputs[1], _ = metric.New("unchanged", map[string]string{}, fields["unchanged"], time.Now())
 
 	lc := Lowercase{}
-	output := lc.Apply(input)
-	assert.Equal(t, 1, len(output))
-	assert.Equal(t, "test", output[0].Name())
+	output := lc.Apply(inputs...)
+	assert.Equal(t, 2, len(output))
+
+	assert.Equal(t, "changed", output[0].Name())
 	assert.Equal(t, map[string]interface{}{
 		"lower_case": "abc123",
 		"upper_case": "ABC123",
 		"mixed_case": "Abc123",
 	}, output[0].Fields())
+
+	assert.Equal(t, "unchanged", output[1].Name())
+	assert.Equal(t, map[string]interface{}{
+		"lower_case": "abc123",
+	}, output[1].Fields())
 }
 
-// With SendOriginals enabled, we send original metrics, and also lowercased metrics
+// With SendOriginals enabled, we send original metrics and also lowercased metrics
 func TestApply_SendOriginals(t *testing.T) {
-	input, err := metric.New(
-		"tEsT",
-		map[string]string{},
-		map[string]interface{}{
-			"lower_case": "abc123",
-			"UPPER_CASE": "ABC123",
-			"Mixed_Case": "Abc123",
-		},
-		time.Now(),
-	)
-	assert.Nil(t, err)
+	inputs := make([]telegraf.Metric, 2)
+	inputs[0], _ = metric.New("ChAnGeD", map[string]string{}, fields["ChAnGeD"], time.Now())
+	inputs[1], _ = metric.New("unchanged", map[string]string{}, fields["unchanged"], time.Now())
 
 	lc := Lowercase{SendOriginal: true}
-	output := lc.Apply(input)
-	assert.Equal(t, 2, len(output))
-	assert.Equal(t, "tEsT", output[0].Name())
-	assert.Equal(t, "test", output[1].Name())
+	output := lc.Apply(inputs...)
+	assert.Equal(t, 3, len(output))
+
+	assert.Equal(t, "ChAnGeD", output[0].Name())
 	assert.Equal(t, map[string]interface{}{
 		"lower_case": "abc123",
 		"UPPER_CASE": "ABC123",
 		"Mixed_Case": "Abc123",
 	}, output[0].Fields())
+
+	assert.Equal(t, "changed", output[1].Name())
 	assert.Equal(t, map[string]interface{}{
 		"lower_case": "abc123",
 		"upper_case": "ABC123",
 		"mixed_case": "Abc123",
 	}, output[1].Fields())
+
+	assert.Equal(t, "unchanged", output[2].Name())
+	assert.Equal(t, map[string]interface{}{
+		"lower_case": "abc123",
+	}, output[2].Fields())
 }
 
 // The following two tests demonstrate that using strings.ContainsAny is ~6
