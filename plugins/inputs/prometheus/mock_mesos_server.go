109ad6ac0a3a2ff9848bc707f9cce2db4754e3a0
Philip Norman <phil@propter.co.uk>
2019-07-12 11:44:33 -0700

    Update to latest mock-mesos-server

--- plugins/inputs/prometheus/mock_mesos_server.go
+++ plugins/inputs/prometheus/mock_mesos_server.go
@@ -1,10 +1,5 @@
 package prometheus
 
-// NOTE: this file relies on protobuf fixtures. These are binary files and
-// cannot readily be changed. We therefore provide the go generate step below
-// which serializes the contents of json files in the testdata directory to
-// protobuf.
-//
 // You should run 'go generate' every time you change one of the json files in
 // the testdata directory, and commit both the changed json file and the
 // changed binary file.
@@ -15,6 +10,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"path/filepath"
 	"testing"
 )
@@ -31,38 +27,43 @@ var (
 // at /api/v1
 func startTestServer(t *testing.T, fixture string) *httptest.Server {
 	router := http.NewServeMux()
+	containers, cOK := loadFixture(t, filepath.Join(fixture, "containers.bin"))
+	state, sOK := loadFixture(t, filepath.Join(fixture, "state.bin"))
+	tasks, tOK := loadFixture(t, filepath.Join(fixture, "tasks.bin"))
+
 	router.HandleFunc("/api/v1", func(w http.ResponseWriter, r *http.Request) {
 		body, _ := ioutil.ReadAll(r.Body)
 
 		w.Header().Set("Content-Type", "application/x-protobuf")
 		w.WriteHeader(http.StatusOK)
-		if bytes.Equal(body, GET_CONTAINERS) {
-			containers := loadFixture(t, filepath.Join(fixture, "containers.bin"))
+		if bytes.Equal(body, GET_CONTAINERS) && cOK {
 			w.Write(containers)
 			return
 		}
-		if bytes.Equal(body, GET_STATE) {
-			state := loadFixture(t, filepath.Join(fixture, "state.bin"))
+		if bytes.Equal(body, GET_STATE) && sOK {
 			w.Write(state)
 			return
 		}
-		if bytes.Equal(body, GET_TASKS) {
-			tasks := loadFixture(t, filepath.Join(fixture, "tasks.bin"))
+		if bytes.Equal(body, GET_TASKS) && tOK {
 			w.Write(tasks)
 			return
 		}
-		t.Fatal("Body contained an unknown request: ", body)
+		t.Errorf("Unknown request to mock-mesos-server: %s", body)
 		return
 	})
 	return httptest.NewServer(router)
 }
 
 // loadFixture retrieves data from a file in ./testdata
-func loadFixture(t *testing.T, filename string) []byte {
+func loadFixture(t *testing.T, filename string) ([]byte, bool) {
 	path := filepath.Join("testdata", filename)
+	if _, err := os.Stat(path); err != nil {
+		// Can't access file - probably not defined
+		return []byte{}, false
+	}
 	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
-	return bytes
+	return bytes, err == nil
 }
